ca0cb3637c86365df3c413dc83c10a40e4c8875d
tafodinho <tafodinho@gmail.com>
2016-04-27 15:21:40 +0100

    new fix

--- storage/framework/views/ec1137feee16d87b1e00cec714d40e35b8b42fff.php
+++ storage/framework/views/ec1137feee16d87b1e00cec714d40e35b8b42fff.php
@@ -6,7 +6,7 @@
                 <div class="panel-heading">Welcome</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    welcome
                 </div>
             </div>
         </div>
